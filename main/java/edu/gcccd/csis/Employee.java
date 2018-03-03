7ed9e0455b816a5cd87f3fb8368bf2d2a96b8090
Christian Galleisky <christiangalleisky@yahoo.com>
2018-03-03 14:24:56 -0800

    First commit, awaiting first overlook

---
+++ main/java/edu/gcccd/csis/Employee.java
@@ -0,0 +1,172 @@
+package edu.gcccd.csis;
+
+public class Employee {
+
+    //instance variables
+    private String firstName;
+    private String lastName;
+    private String jobTitle;
+    private String orgOneWorksFor;
+    private int bDay; private int bMonth; private int bYear;
+    private boolean isAlive;
+
+    public Employee(String firstName, String lastName, String jobTitle, String orgOneWorksFor,
+                    int bDay, int bMonth, int bYear, boolean isAlive) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.jobTitle = jobTitle;
+        this.orgOneWorksFor = orgOneWorksFor;
+        this.bDay = bDay;
+        this.bMonth = bMonth;
+        this.bYear = bYear;
+        this.isAlive = isAlive;
+    }
+
+    public Employee (){
+        this.firstName=null;
+        this.lastName=null;
+        this.jobTitle=null;
+        this.orgOneWorksFor=null;
+        this.bDay=0;
+        this.bMonth=0;
+        this.bYear=0;
+        this.isAlive=false;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getOrgOneWorksFor() {
+        return orgOneWorksFor;
+    }
+
+    public void setOrgOneWorksFor(String orgOneWorksFor) {
+        this.orgOneWorksFor = orgOneWorksFor;
+    }
+
+    public int getbDay() {
+        return bDay;
+    }
+
+    public void setbDay(int bDay) {
+        this.bDay = bDay;
+    }
+
+    public int getbMonth() {
+        return bMonth;
+    }
+
+    public void setbMonth(int bMonth) {
+        this.bMonth = bMonth;
+    }
+
+    public int getbYear() {
+        return bYear;
+    }
+
+    public void setbYear(int bYear) {
+        this.bYear = bYear;
+    }
+
+    public boolean getIsAlive() {
+        return isAlive;
+    }
+
+    public void setIsAlive(boolean alive) {
+        this.isAlive = alive;
+    }
+
+    public boolean getIsOldEnoughToWork(int day, int month, int year){
+        if((year-this.bYear>=16)||(year-this.bYear==15 && month-this.bMonth>=1)||(year-this.bYear==15 &&
+            month-this.bMonth==0 && day-this.bDay>=0)){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        Employee e = (Employee) o;
+        if(this.firstName.equals(e.getFirstName()) && this.lastName.equals(e.getLastName()) &&
+                this.getJobTitle().equals(e.getJobTitle()) && this.orgOneWorksFor.equals(e.orgOneWorksFor)
+                && this.bDay==e.bDay && this.bMonth==e.bMonth && this.bYear==e.bYear && this.isAlive==e.isAlive)
+        {return true;}
+        else return false;
+    }
+
+    @Override
+    public String toString(){
+        return String.format(this.firstName+" "+this.lastName+" is a(n) "+this.jobTitle+" @ "+this.orgOneWorksFor
+                                +"\nborn on: "+this.bMonth+"\\"+this.bDay+"\\"+this.bYear);
+    }
+
+
+
+    public class Organization {
+
+        //instance variables
+        private String orgName;
+        private int numberOfEmployees;
+
+        public Organization(String orgName, int numberOfEmployees){
+            this.orgName=orgName;
+            this.numberOfEmployees=numberOfEmployees;
+        }
+
+        public Organization(){
+            this.orgName=null;
+            this.numberOfEmployees=0;
+        }
+
+        public String getOrgName() {
+            return orgName;
+        }
+
+        public void setOrgName(String orgName) {
+            this.orgName = orgName;
+        }
+
+        public int getNumberOfEmployees() {
+            return numberOfEmployees;
+        }
+
+        public void setNumberOfEmployees(int numberOfEmployees) {
+            this.numberOfEmployees = numberOfEmployees;
+        }
+
+        @Override
+        public boolean equals(Object o){
+            Employee.Organization org1= (Employee.Organization) o;
+            if(this.orgName.equals(org1.getOrgName()) && this.numberOfEmployees==org1.getNumberOfEmployees()){
+                return true;
+            }
+            else return false;
+        }
+
+        @Override
+        public String toString(){
+            return String.format("Company: "+orgName+"\n"+"Number of Employees: "+numberOfEmployees);
+        }
+    }
+}
---
+++ test/java/edu/gcccd/csis/StructureTest.java
@@ -0,0 +1,91 @@
+package edu.gcccd.csis;
+
+import org.junit.Test;
+import org.junit.Before;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNull;
+
+/**
+ * Tests for a system that can track employee information for two organizations.
+ * The Employee information you must (at least) track, is as follows:
+ * Name, Job Title, Organization they work for, Birthday
+ *
+ * As for the Organization that the Employee works for, you must also (at the very least) track this information:
+ * Organization Name, Number of Employees
+ *
+ * The system must be able to properly compare any two employees against each other to determine,
+ * if they are the same Person. This means that if you compared two People,
+ * with the same Name, Birthday, and Organization, the system should think that they are equal to one another.
+ * If any of these properties are different, then the two People are not the same Person.
+ * The same rules apply to comparing Organizations to one another.
+ * Organizations with the same Organization name are to be thought of as equal,
+ * different names means different organizations.
+ */
+public class StructureTest {
+
+
+
+    @Test
+    public void employeeComplete() {
+        //
+        // check that an employee's attribute are complete and make sense.
+        // assertTrue( employee is alive and company complies with child labor laws ...;
+        //...
+        Employee e1 = new Employee("Chris", "Galleisky", "Programmer",
+                                    "Microsoft", 16, 3, 1990, true);
+        System.out.println(e1.toString());
+        assertTrue(e1.getIsAlive());
+        assertTrue(e1.getIsOldEnoughToWork(2,3,2018));
+    }
+    /**
+     * Check Employee Equality
+     */
+    @Test
+    public void employeeEq() {
+        Employee e1 = new Employee("Chris", "Galleisky", "Programmer",
+                                    "Microsoft", 16, 3, 1990, true);
+        Employee e2 = new Employee("Chris", "Galleisky", "Programmer",
+                                    "Microsoft", 16, 3, 1990, true);
+
+        // Create employee e1,
+        // Create another employee e2 that matches the specific criteria for equality
+        assertTrue(e1.equals(e2));
+
+        // modify employee e2 (eg. by making her/him join the other company
+        e2.setOrgOneWorksFor("Google");
+        assertNotEquals(e1, e2);
+
+    }
+
+    /**
+     * Check Company Equality
+     */
+    @Test
+    public void companyEq() {
+
+        Employee e1 = new Employee("Chris", "Galleisky", "Programmer",
+                                    "Microsoft", 16, 3, 1990, true);
+        // Create company c1,
+        // Create company employee c2 that matches the specific criteria for equality
+        Employee.Organization c1 = e1.new Organization("Microsoft",10000);
+        Employee.Organization c2 = e1.new Organization("Microsoft",10000);
+        assertTrue(c1.equals(c2));
+
+        // modify company c2, i.e. change its name or create another company with a different name ..
+        c2.setOrgName("Google");
+        assertNotEquals(c1, c2);
+
+    }
+
+    @Test
+    public void companyComplete() {
+        // check that a companies' attribute are complete and make sense.
+        Employee e1 = new Employee("Chris", "Galleisky", "Programmer",
+                                    "Microsoft", 16, 3, 1990, true);
+        Employee.Organization c1 = e1.new Organization("Microsoft",10000);
+        System.out.println(c1.toString());
+    }
+}
